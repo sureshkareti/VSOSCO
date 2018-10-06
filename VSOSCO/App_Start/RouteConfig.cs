52594b041d77b7ca32fe4da1a742649e1cfeef24
Madhubabu <madhubabu559@gmail.com>
2018-10-06 16:52:31 +0530

    added pages

--- VSOSCO/App_Start/RouteConfig.cs
+++ VSOSCO/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace VSOSCO
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "RunShark", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
